db9719b616b5972849fd032d1aceccaef709b47c
sheldondcosta <ssheldondcosta@gmail.com>
2024-06-08 12:53:09 -0400

    ViewModel

--- app/src/main/java/com/example/shoppingappjetpackcompose/ui/viewmodel/CartViewModel.kt
+++ app/src/main/java/com/example/shoppingappjetpackcompose/ui/viewmodel/CartViewModel.kt
@@ -7,14 +7,15 @@ import androidx.lifecycle.viewModelScope
 import com.example.shoppingappjetpackcompose.data.model.product.ProductsItemModel
 import com.example.shoppingappjetpackcompose.data.repository.ShoppingProductRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class CartViewModel @Inject constructor(private val repository: ShoppingProductRepository) : ViewModel() {
 
-    private val _products = MutableLiveData<List<ProductsItemModel>>()
-    val products: LiveData<List<ProductsItemModel>> = _products
+    private val _products = MutableLiveData<Flow<List<ProductsItemModel?>?>>()
+    val products: LiveData<Flow<List<ProductsItemModel?>?>> = _products
 
     private val _selectedProduct = MutableLiveData<ProductsItemModel?>()
     val selectedProduct: LiveData<ProductsItemModel?> = _selectedProduct
